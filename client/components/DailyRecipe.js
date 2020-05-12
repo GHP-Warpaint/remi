78793f4848ad63f08937d7e4d9d72f70375fbc1e
Michelle <michelle_broomes@yahoo.com>
2020-05-12 16:25:20 -0400

    daily recipe updated in navbar to be at homepage link

---
+++ client/components/DailyRecipe.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class DailyRecipe extends Component {
+  // constructor(){
+  //   super()
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>Our Suggested Recipe of the Day!</h1>
+      </div>
+    )
+  }
+}
+
+export default DailyRecipe

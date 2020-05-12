2fae1772834d6e3508524eb6c4367d2b480a4ab1
Michelle <michelle_broomes@yahoo.com>
2020-05-12 12:33:14 -0400

    got rid of err causing methods

--- client/store/recipe.js
+++ client/store/recipe.js
@@ -28,21 +28,12 @@ export const fetchRecipe = () => async dispatch => {
     const API_KEY = '2905bb46b5ea4b48aa1c8c6e3a434a6f'
     const INGREDIENT_LIST = ['bananas', 'apples', 'cheese', 'crackers']
     let requestString = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY}&ingredients=`
-    // const ingredientsString = INGREDIENT_LIST.map(ingredient =>
-    //    ingredient + ',+'
-    // );
+
     const ingredientsString = INGREDIENT_LIST.join(',+')
     requestString = requestString + ingredientsString
     console.log('THUNK REQUEST STRING=>', requestString)
     const returnReq = await axios.get(requestString)
-    console
-      .log('THUNK RETURN REQUEST', returnReq)
-      // .header("X-RapidAPI-Key",  API_KEY)
-      .end(function(result) {
-        if (result.status === 200) {
-          console.log('RESULT BODY', result.body)
-        }
-      })
+    console.log('THUNK RETURN REQUEST', returnReq)
   } catch (err) {
     console.error(err)
   }

6c21591323f4badc97ae73a112fe597200ba6449
Allison Sasso <aqsasso@gmail.com>
2020-05-13 17:20:46 -0400

    feat add fridge item working, submit clears box

--- client/components/AddFoodItem.js
+++ client/components/AddFoodItem.js
@@ -24,6 +24,7 @@ class AddFoodItem extends React.Component {
     const {addFood} = this.props
     const food = this.state
     await addFood(food)
+    this.setState({name: ''})
   }
 
   render() {

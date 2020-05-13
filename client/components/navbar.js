3cae2227d7e37fd9b2fb0da41bf441d9f20d1548
Shannon <scrowley016@gmail.com>
2020-05-12 22:53:24 -0400

    user home fully functional

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -14,6 +14,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
           <Link to="/fridge">Fridge</Link>
+          <Link to="/account">My Account</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>

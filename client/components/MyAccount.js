1699a49e2b3caceb1e134447fbef773217fa31b8
Shannon <scrowley016@gmail.com>
2020-05-13 11:11:53 -0400

    fixed error on master

--- client/components/MyAccount.js
+++ client/components/MyAccount.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {me} from '../store/user'
+import {me} from '../reducer/user'
 
 export class Fridge extends React.Component {
   constructor(props) {

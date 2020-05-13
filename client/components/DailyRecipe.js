c2f7a0253093bd5065014fafb04718c8f7b8d734
Michelle <michelle_broomes@yahoo.com>
2020-05-12 21:40:05 -0400

    got rid of some unneccessary code

--- client/components/DailyRecipe.js
+++ client/components/DailyRecipe.js
@@ -3,24 +3,21 @@ import {connect} from 'react-redux'
 import {getDailyRecipe} from '../reducer/dailyRecipe'
 
 class DailyRecipe extends Component {
-  // constructor(){
-  //   super()
-  // }
   componentDidMount() {
     this.props.getDailyRecipeInfo()
   }
 
   render() {
-    const randomNum = this.props.dailyRec.dailyRecipe.length
-      ? Math.floor(Math.random() * this.props.dailyRec.dailyRecipe.length)
-      : 6
+    const randomNum =
+      this.props.dailyRec.dailyRecipe.length &&
+      Math.floor(Math.random() * this.props.dailyRec.dailyRecipe.length)
     const randomChoice = this.props.dailyRec.dailyRecipe[1]
       ? this.props.dailyRec.dailyRecipe[randomNum]
       : 'loads'
     return (
       <div>
         <h1>Our Suggested Recipe of the Day!</h1>
-        {randomChoice !== 'loads' ? (
+        {randomChoice !== 'loads' && (
           <div>
             <h3>{randomChoice.title}</h3>
             <img src={randomChoice.imageUrl} height="300" />
@@ -35,8 +32,6 @@ class DailyRecipe extends Component {
               </a>
             </p>
           </div>
-        ) : (
-          'loading'
         )}
         {/* the following returns a list of all available daily recipes */}
         {/* {this.props.dailyRec.dailyRecipe

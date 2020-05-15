3a8c7e3b30c5883f0eb6a3677a2f09b60ceac54e
Allison Sasso <aqsasso@gmail.com>
2020-05-15 00:39:38 -0400

    style added, but issues with addign food to user

--- client/components/fridge.js
+++ client/components/fridge.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import AddFoodItem from './AddFoodItem'
 import {fetchFood, deleteFood} from '../reducer/fridge'
 
@@ -27,16 +27,27 @@ export class Fridge extends React.Component {
       <div id="fridge">
         <h1>WELCOME TO THE FRIDGE!</h1>
         <div>
-          <AddFoodItem />
-
+          <div>
+            <AddFoodItem />
+            <Link to="/recipe" className="fakeButton">
+              Let's Cook!
+            </Link>
+            {/* <button type="button" onClick={() => history.push('/recipe')}>
+              Let's Cook!
+            </button> */}
+          </div>
           {this.props.food &&
             this.props.food.map(food => (
               <div key={food.id} className="item">
-                <img src={food.imageUrl} height="100px" width="auto" />
-                {food.name}
-                <button type="submit" onClick={() => this.removeFood(food.id)}>
+                <button
+                  className="itemBtn"
+                  type="submit"
+                  onClick={() => this.removeFood(food.id)}
+                >
                   X
                 </button>
+                <img src={food.imageUrl} height="100px" width="auto" />
+                {food.name}
               </div>
             ))}
         </div>

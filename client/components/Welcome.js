6fb956d9fa4af028350936dc469b908041b22781
Michelle <michelle_broomes@yahoo.com>
2020-05-16 02:49:14 -0400

    welcome pg summary cleaned up a bit

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -7,12 +7,12 @@ const Welcome = () => {
       <h1 className="welcome">Introducing, Chef Remy</h1>
 
       <h5>
-        Meet Chef Remy -- Your personal assistant chef Discover and cook new
-        recipes. Receive recipe suggestions based on ingredients in stock. Chef
-        Remy is able to talk to you via a companion Alexa Skill or through the
-        website enabled speech recognition and audio (Users must click to talk).
-        Once a recipe is decided on, the application should walk users
-        step-by-step through cooking the dish.
+        Meet Chef Remy -- Your personal assistant chef. Be prepared to discover
+        and cook new tasty recipes. You will receive recipe suggestions based on
+        ingredients in your kitchen. Chef Remy is interactive. He can talk to
+        you via a companion Alexa Skill or through the website enabled speech.
+        Once you decide on a recipe, Chef Remy will take you step-by-step
+        through the meal preparation.
       </h5>
 
       <Link to="/about-alexa">

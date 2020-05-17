40fa07323ca5d8ecc03a900d6b7c1d62439fe6a1
Michelle <michelle_broomes@yahoo.com>
2020-05-16 23:31:51 -0400

    got countdown to display on receipts

--- client/components/Receipt.js
+++ client/components/Receipt.js
@@ -1,6 +1,38 @@
 import React, {Component} from 'react'
 
 export default class Receipt extends Component {
+  constructor() {
+    super()
+    this.state = {
+      display: ''
+    }
+    this.counter = this.counter.bind(this)
+  }
+
+  counter() {
+    let countDownDate = new Date('May 31, 2020 15:30:00').getTime()
+
+    const countdownfunction = setInterval(() => {
+      let rightNow = new Date().getTime()
+      let difference = countDownDate - rightNow
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24))
+      const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      )
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000)
+
+      if (difference < 0) {
+        clearInterval(countdownfunction)
+        this.setState({display: 'EXPIRED'})
+      }
+
+      this.setState({
+        display: `${days}d ${hours}h ${minutes}m ${seconds}s`
+      })
+    }, 1000)
+  }
+
   render() {
     return (
       <div>
@@ -9,6 +41,14 @@ export default class Receipt extends Component {
           We'll return your itemized list for you and you can decide if you'd
           like to add the items to your fridge
         </h3>
+        <div className="middle">
+          <h1>COMING SOON</h1>
+          <hr />
+          <p id="countdown">{this.state.display}</p>
+          <button type="button" onClick={this.counter}>
+            Click Here
+          </button>
+        </div>
       </div>
     )
   }

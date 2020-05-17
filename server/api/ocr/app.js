e7b02175526ce0b502be552fae2389bcd9651998
Michelle <michelle_broomes@yahoo.com>
2020-05-16 22:19:13 -0400

    set up backend receipt files in server/ocr folder. need to connect it to frontend recept file

--- server/api/ocr/app.js
+++ server/api/ocr/app.js
@@ -8,11 +8,11 @@ const worker = new TessaractWorker()
 
 //cb === callback
 const storage = multer.diskStorage({
-  destination: (req, res, cb) => {
+  destination: (req, file, cb) => {
     cb(null, './uploads')
   },
-  filename: (req, res, cb) => {
-    cb(null, req.file)
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
   }
 })
 
@@ -27,4 +27,29 @@ app.set('view engine', 'ejs')
 
 app.get('/api/receipt', (req, res) => {
   res.render('index')
+})
+
+app.post('/upload', (req, res) => {
+  upload(req, res, err => {
+    if (err) return console.log('this is the problem =>', err)
+
+    fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+      if (err) return console.log('this is the problem =>', err)
+
+      worker
+        .recognize(data, 'eng', {tessjs_create_pdf: '1'})
+        .progress(progress => {
+          console.log(progress)
+        })
+        .then(result => {
+          res.redirect('/download')
+        })
+        .finally(() => worker.terminate())
+    })
+  })
+})
+
+app.get('/download', (req, res) => {
+  const file = `${__dirname}/tesseract.js-ocr-result.pdf`
+  res.download(file)
 })

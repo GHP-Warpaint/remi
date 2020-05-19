c5796b271993108486625739192cfa09acdf7fb0
Nora Lashner <nlashner@gmail.com>
2020-05-19 17:51:01 -0400

    chef remy hat now directs to '/'

---
+++ client/components/SingleRecipe.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchRecipeDirections} from '../reducer/recipe'
+
+class SingleRecipe extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    if (!this.props.directions[0]) return <h1>loading </h1>
+
+    const directions = this.props.directions[0].steps
+
+    const name = this.props.history.location.recipeProps.name
+    const imageURL = this.props.history.location.recipeProps.image
+
+    return (
+      <div>
+        <h1>{name}</h1>
+        <img src={imageURL} />
+        {directions.map(step => {
+          return (
+            <p key={step.number}>
+              {step.number}. {step.step}
+            </p>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+const mapState = state => {
+  return {
+    food: state.fridge.food,
+    recipe: state.recipe.recipe,
+    directions: state.recipe.directions
+  }
+}
+
+const mapDispatch = dispatch => ({
+  fetchDirections: id => dispatch(fetchRecipeDirections(id))
+})
+
+export default connect(mapState, mapDispatch)(SingleRecipe)
